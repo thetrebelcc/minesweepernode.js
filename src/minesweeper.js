6ffbabf65dbf073bff02b8a48e705aff6a449569
Fabian Anguiano <fabiananguiano@gmail.com>
2018-07-31 21:34:41 -0500

    first commit

---
+++ src/minesweeper.js
@@ -0,0 +1,18 @@
+const blankLine = '  |   |  ';
+
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+
+
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+
+
+console.log('this is what a live board would be');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
+
