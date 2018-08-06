4e0da9c7672c4917e72e74b10acab0bf82bd0df7
Fabian Anguiano <fabiananguiano@gmail.com>
2018-08-06 12:29:23 -0500

    refactoring

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,30 @@
-const blankLine = '  |   |  ';
+// const blankLine = '  |   |  ';
 
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-
-
-
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+// console.log('This is what an empty board would look like:');
+// console.log(blankLine);
+// console.log(blankLine);
+// console.log(blankLine);
 
 
-console.log('this is what a live board would be');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
 
+// const guessLine = '1 |   |  ';
+// const bombLine = '  | B |  ';
+
+
+// console.log('this is what a live board would be');
+// console.log(guessLine);
+// console.log(bombLine);
+// console.log(blankLine);
+
+var board = [
+
+    ['','',''],
+    ['','',''],
+    ['','','']
+];
+
+console.log(board);
+
+const printBoard = board => {
+
+};
