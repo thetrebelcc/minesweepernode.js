28cb32b85ce5cac89842cb89a11dda16c23af4e7
Fabian Anguiano <fabiananguiano@gmail.com>
2018-08-06 22:16:17 -0500

    refactored

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -16,15 +16,26 @@
 // console.log(bombLine);
 // console.log(blankLine);
 
-var board = [
 
-    ['','',''],
-    ['','',''],
-    ['','','']
+
+const exampleBoard = [
+
+    [' ',' ',' '],
+    [' ',' ',' '],
+    [' ',' ',' ']
 ];
 
-console.log(board);
 
-const printBoard = board => {
+const printBoard = (board) => {
+console.log('current game');
+console.log(board[0].join(' | '));
+console.log(board[1].join(' | '));
+console.log(board[2].join(' | '));
+
 
 };
+
+printBoard(exampleBoard);
+exampleBoard[0][1] ="1"
+exampleBoard[1][0] ="B"
+printBoard(exampleBoard);
