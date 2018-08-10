8c9533d288190a1e24cce2c5e280851b75dd9a64
Fabian Anguiano <fabiananguiano@gmail.com>
2018-08-10 15:59:00 -0500

    dynamically genarate nbaords

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,41 +1,47 @@
-// const blankLine = '  |   |  ';
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    const board = [];
 
-// console.log('This is what an empty board would look like:');
-// console.log(blankLine);
-// console.log(blankLine);
-// console.log(blankLine);
-
-
-
-// const guessLine = '1 |   |  ';
-// const bombLine = '  | B |  ';
-
-
-// console.log('this is what a live board would be');
-// console.log(guessLine);
-// console.log(bombLine);
-// console.log(blankLine);
-
-
-
-const exampleBoard = [
-
-    [' ',' ',' '],
-    [' ',' ',' '],
-    [' ',' ',' ']
-];
-
-
-const printBoard = (board) => {
-console.log('current game');
-console.log(board[0].join(' | '));
-console.log(board[1].join(' | '));
-console.log(board[2].join(' | '));
-
-
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        const row = [];
+        for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
 };
 
-printBoard(exampleBoard);
-exampleBoard[0][1] ="1"
-exampleBoard[1][0] ="B"
-printBoard(exampleBoard);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    const board = [];
+
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        const row = [];
+        for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
+
+    let numberOfBombsPlaced = 0;
+
+    while (numberOfBombsPlaced < numberOfBombs) {
+        const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
+
+    return board;
+};
+
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3, 3);
+let bombBoard = generateBombBoard(3, 3, 3);
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board:');
+
+printBoard(bombBoard);
